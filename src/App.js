c0937ff16aa390fad3c2512c938a36c37003c6b0
Zuz <hello@zuz.com.pk>
2020-11-17 14:51:45 +0500

    Updated to Card Component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-<<<<<<< HEAD
 import { 
   Button,
   Card,
   Switch
 } from "./zuz";
-=======
-import { Button, Switch } from "./zuz";
->>>>>>> 4abb6e30027a442b9f367a1e2c5f386461f653fd
 
 function App() {
   return (
     <div className="App abs abc">
-<<<<<<< HEAD
       <Card />
       {/*<Switch onChange={checked => console.log(checked)} checked={true} ui="ios" /><br /><br />
       <Switch onChange={checked => console.log(checked)} checked={true} ui="android" />
       <Button label="Click me" onClick={evt=>console.log("I have been clicked")} />
-=======
-      <Switch onChange={checked => console.log(checked)} checked={true} ui="ios" /><br /><br />
-      <Switch onChange={checked => console.log(checked)} checked={true} ui="android" />
-      {/*<Button label="Click me" onClick={evt=>console.log("I have been clicked")} />
->>>>>>> 4abb6e30027a442b9f367a1e2c5f386461f653fd
       <Button label="Email Sent" type="ok" icon="icon-check" onClick={evt=>console.log("I have been clicked")} />
       <Button label="Error" type="error" icon="icon-cross" onClick={evt=>console.log("I have been clicked")} />*/}
     </div>
